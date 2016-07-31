42dd33f3e948dc6832d7768b7f06fc4dd4cf6864
fernandorosero <tatofarp@hotmail.com>
2016-07-31 14:11:01 +0200

    commit toshiba

--- app/src/main/java/kidlocaliza/beacon/thelastmonkey/com/pruebagithub/MainActivity.java
+++ app/src/main/java/kidlocaliza/beacon/thelastmonkey/com/pruebagithub/MainActivity.java
@@ -2,12 +2,26 @@ package kidlocaliza.beacon.thelastmonkey.com.pruebagithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnHola;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnHola = (Button)findViewById(R.id.btnHola);
+        System.out.println("Funciona el boton");
+        Toast.makeText(getApplicationContext(),"Hola saludos",Toast.LENGTH_SHORT).show();
+        btnHola.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                System.out.println("Funciona el boton");
+            }
+        });
     }
 }
